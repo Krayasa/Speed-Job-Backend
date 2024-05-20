cacd623bcabfa67251b5d5d1720694519be2b5b0
aarajbhattarai <aarajbhattarai@gmail.com>
2024-05-21 03:14:52 +0545

    User model created

--- src/customuser/models.py
+++ src/customuser/models.py
@@ -1,5 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from customuser.managers import CustomUserManager
+
+GENDER_CHOICES = (("male", "Male"), ("female", "Female"))
+ROLE_CHOICES = (
+    ("employer", "employer"),
+    ("employee", "employee"),
+)
+
+class User(AbstractUser):
+    role = models.CharField(choices=ROLE_CHOICES,error_messages={"required": "Role must be provided"})
+    gender = models.CharField(max_length=10, blank=True, null=True, default="")
+    email = models.EmailField(
+        unique=True, blank=False, error_messages={"unique": "A user with that email already exists."}
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 
-class User(AbstractUser):  # type: ignore[django-manager-missing]
-    pass
+    def __unicode__(self):
+        return self.email
+
+    objects = CustomUserManager()
